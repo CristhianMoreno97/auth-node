e7ac8617fa9426e0496146590339179f2fc7421f
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-14 14:49:01 -0500

    refactor(app): Restructure app initialization and routing
    - Reorganized the initialization of Express application to improve code readability and organization.
    - Moved the configuration of CORS and JSON parsing middleware to the top for clarity and consistency.
    - Changed route handling to use app.use() method for better scalability and maintainability.
    - Updated route paths to follow a more organized structure ("/api/<endpoint>") for clarity and consistency.
    - Added route handlers for '/api/signup', '/api/login', '/api/user', '/api/todos', '/api/refresh-token', and '/api/signout'.
    - This restructuring enhances the modularity and scalability of the application by separating concerns and following best practices for routing and middleware usage.

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 require('dotenv').config()
+const port = process.env.PORT || 5000
 
 const app = express()
-const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/signup', require('./routes/signup'))
+app.use('/api/login', require('./routes/login'))
+app.use('/api/user', require('./routes/user'))
+app.use('/api/todos', require('./routes/todos'))
+app.use('/api/refresh-token', require('./routes/refreshToken'))
+app.use('/api/signout', require('./routes/signout'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
